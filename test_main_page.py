0ce05c91ce90b073ac54bc7c29b003bae55100be
Alex <alexandra.bulgak@gmail.com>
2024-03-10 20:49:38 +0100

    login_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,10 @@
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
-
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.go_to_login_page()
-    page.should_be_login_link()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
