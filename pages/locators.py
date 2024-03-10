9af7abdc08ee06ba1bfd79d301fb6be495f93dcf
Alex <alexandra.bulgak@gmail.com>
2024-03-10 23:22:47 +0100

    check that good was added

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,16 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CLASS_NAME, "btn-add-to-basket")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div")
+    PRODUCT_NAME_IN_MESSAGE = (By.CSS_SELECTOR, '#messages > div:nth-child(1) > div > strong')
+    # PRODUCT_NAME = (By.CSS_SELECTOR, '.product_main > h1')
+    # PRODUCT_PRICE = (By.CSS_SELECTOR, '.product_main > p.price_color')
+    BASKET_TOTAL = (By.CSS_SELECTOR, '.alertinner > p:nth-child(1) > strong')
+    MESSAGE_BASKET_TOTAL = (By.CSS_SELECTOR, '.alertinner > p:nth-child(1)')
+
+
+
---
+++ pages/product_page.py
@@ -0,0 +1,37 @@
+from selenium.webdriver.common.by import By
+
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_to_cart(self):
+        self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON).click()
+
+    def get_product_name(self):
+        return self.browser.find_element(By.CSS_SELECTOR, '.product_main > h1').text
+
+    def get_price(self):
+        return self.browser.find_element(By.CSS_SELECTOR, '.product_main > p.price_color').text
+
+    def good_was_added(self):
+        self.should_be_message_of_success()
+        self.product_name_should_be_in_message(self.get_product_name())
+        self.should_be_message_basket_total()
+        self.price_in_basket_matches_product_price(self.get_price())
+
+    def should_be_message_of_success(self):
+        assert "has been added to your basket." in self.browser.find_element(
+            *ProductPageLocators.SUCCESS_MESSAGE).text, "Wrong message about adding product to the card or the message is missing"
+
+    def product_name_should_be_in_message(self, product_name):
+        assert product_name == self.browser.find_element(
+            *ProductPageLocators.PRODUCT_NAME_IN_MESSAGE).text, "Wrong product name in the message"
+
+    def should_be_message_basket_total(self):
+        assert "Your basket total is now" in self.browser.find_element(
+            *ProductPageLocators.MESSAGE_BASKET_TOTAL).text, "Wrong message about basket total or the message is missing"
+
+    def price_in_basket_matches_product_price(self, price):
+        assert price == self.browser.find_element(
+            *ProductPageLocators.BASKET_TOTAL).text, "The price doesn't match"
